8d76aa429301f69448365e16cef1358a1cc76354
sclearner <71807587+sclearner@users.noreply.github.com>
2024-04-10 13:20:24 +0700

    feat: Add instrument

--- models/index.js
+++ models/index.js
@@ -2,6 +2,8 @@ import config from '../configs/db.config.js'
 
 import {Sequelize, DataTypes} from 'sequelize';
 import TraderModel from './trader.model.js';
+import { RefreshTokenModel } from './refreshToken.model.js';
+import { InstrumentModel } from './instrument.model.js';
 
 const sequelize = new Sequelize(
     config.DB,
@@ -9,15 +11,36 @@ const sequelize = new Sequelize(
     config.PASSWORD,
     {
         host: config.HOST,
-        dialect: config.dialect
+        dialect: config.dialect,
+        // logging: false
     }
 )
 
 const Trader = TraderModel(sequelize, DataTypes);
-const db = {
-    sequelize, 
-    Sequelize, 
-    Trader,
+const RefreshToken = RefreshTokenModel(sequelize, DataTypes);
+const Instrument = InstrumentModel(sequelize, DataTypes);
+class db {
+    static sequelize = sequelize
+    static Sequelize = Sequelize
+    static Trader = Trader
+    static RefreshToken = RefreshToken
+    static Instrument = Instrument
 }
 
-export {db, Sequelize, Trader};
+db.RefreshToken.belongsTo(db.Trader, {
+    foreignKey: 'traderId', targetKey: 'id'
+});
+
+db.Trader.hasOne(db.RefreshToken, {
+    foreignKey: 'traderId', targetKey: 'id'
+})
+
+db.Instrument.belongsToMany(db.Trader, {
+    through: 'trader_balance'
+})
+
+// db.Trader.afterInsert(
+
+// )
+
+export {db, Sequelize, Trader, RefreshToken, Instrument}
---
+++ models/instrument.model.js
@@ -0,0 +1,21 @@
+export function InstrumentModel(sequelize, DataTypes) {
+    const Instrument = sequelize.define("instrument",
+        {symbol: {
+            type: DataTypes.STRING,
+            primaryKey: true
+        },
+        currency: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        last_price: {
+            type: DataTypes.DECIMAL(20, 2),
+            allowNull: true
+        }
+    }, {
+        updatedAt: false
+    }
+    )
+
+    return Instrument
+}
